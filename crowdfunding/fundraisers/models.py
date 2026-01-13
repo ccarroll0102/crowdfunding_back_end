1375965d6bd605e996911bb7e11924542f38c968
ccarroll0102 <claudiamaycarroll@gmail.com>
2026-01-13 19:45:31 +1100

    wrote my first django model

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+#we are setting up our database models here and telling django what fields and what type of information for each field we want each model to have
 
 # Create your models here.
+class Fundraiser(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    goal = models.IntegerField()
+    image = models.URLField()
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    
