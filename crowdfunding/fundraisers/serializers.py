37a7dc6110257246313bcc45e1b63b3bf6cae6e6
ccarroll0102 <claudiamaycarroll@gmail.com>
2026-01-20 20:28:47 +1100

    migrations and more creating APIS in class!

---
+++ crowdfunding/fundraisers/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from django.apps import apps
+
+class FundraiserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = apps.get_model('fundraisers.Fundraiser')
+        fields = '__all__'
+
+class PledgeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = apps.get_model('fundraisers.Pledge')
+        fields = '__all__'
