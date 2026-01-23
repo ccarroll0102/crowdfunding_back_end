74425bd3e1a4a05391bc881d57b28c086c03f456
ccarroll0102 <claudiamaycarroll@gmail.com>
2026-01-24 10:45:01 +1100

    creating a fundraiser detail serializer

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from .models import Fundraiser
-from .serializers import FundraiserSerializer
+from .serializers import FundraiserDetailSerializer, FundraiserSerializer, FundraiserDetailSerializer
 from .models import Pledge
 from .serializers import PledgeSerializer
 
@@ -34,7 +34,7 @@ class FundraiserDetail(APIView):
 
     def get(self, request, pk):
         fundraiser = get_object_or_404(Fundraiser, pk=pk)
-        serializer = FundraiserSerializer(fundraiser)
+        serializer = FundraiserDetailSerializer(fundraiser)
         return Response(serializer.data)
     
 class PledgeList(APIView):
