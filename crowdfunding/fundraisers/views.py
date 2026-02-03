c8c0fc610b2227cab735e39e590febd9e4105cdc
ccarroll0102 <claudiamaycarroll@gmail.com>
2026-02-03 15:47:41 +1100

    installing user permissions

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -1,16 +1,18 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.generics import get_object_or_404
+from rest_framework import status, permissions
+from .permissions import IsOwnerOrReadOnly
 from .models import Fundraiser
 from .serializers import FundraiserDetailSerializer, FundraiserSerializer, FundraiserDetailSerializer
 from .models import Pledge
 from .serializers import PledgeSerializer
 
 class FundraiserList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         fundraisers = Fundraiser.objects.all()
@@ -32,10 +34,39 @@ class FundraiserList(APIView):
     
 class FundraiserDetail(APIView):
 
+    permission_classes = [
+      permissions.IsAuthenticatedOrReadOnly,
+      IsOwnerOrReadOnly
+   ]
+
+    def get_object(self, pk):
+        try:
+            fundraiser = Fundraiser.objects.get(pk=pk)
+            self.check_object_permissions(self.request, fundraiser)
+            return fundraiser
+        except Fundraiser.DoesNotExist:
+            raise Http404
+
     def get(self, request, pk):
         fundraiser = get_object_or_404(Fundraiser, pk=pk)
         serializer = FundraiserDetailSerializer(fundraiser)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        fundraiser = self.get_object(pk)
+        serializer = FundraiserDetailSerializer(
+            instance = fundraiser,
+            data = request.data, 
+            partial = True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
     
 class PledgeList(APIView):
     def get(self, request):
